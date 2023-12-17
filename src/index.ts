54a01e17662f131328dbf0a2e90664174b9642e9
az <az.dev@e.mail.de>
2023-12-17 19:04:11 +0100

    add vue router

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./views/Home.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: "/", name: "Home", component: Home }],
+});
+export default router;
